1aa57797217b6616edc0c24a2b89c75d37eb97f4
akitayuri <nmnl_y.bgux@icloud.com>
2021-07-20 03:10:18 +0900

    add gem devise rexml

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :reviews
   resources :blogs
+  root 'blogs#index' 
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
